4348d1462aec5c46897746c19b65ebc0dac226cb
jkusa-jr <43914629+jkusa-jr@users.noreply.github.com>
2025-03-05 21:45:34 -0600

    Add files via upload

--- slideshow/index.js
+++ slideshow/index.js
@@ -2,45 +2,43 @@ function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-  }
-
-
+}
 
 async function updateImg() {
     let urlParams = new URLSearchParams(document.location.search);
-
-
-    var prompt = urlParams.get('prompt')
+    let prompt = urlParams.get('prompt')
     try {
 
         //attach img element to document body
-        let imgURL = `https://image.pollinations.ai/prompt/${prompt}?seed=${getRandomInt(0,1000000)}&width=${screen.width}&height=${screen.height}&nologo=true&private=true&enhance=false&safe=true`;
+        let imgURL = `https://image.pollinations.ai/prompt/${prompt}?seed=${getRandomInt(0, 1000000)}&width=${window.innerWidth}&height=${window.innerHeight}&nologo=true&private=true&enhance=false&safe=true`;
         console.log(imgURL)
         await fetch(imgURL)
-        .then(response => response.arrayBuffer())
-        .then(buffer => {
-            let base64String = btoa(
-                new Uint8Array(buffer)
-                    .reduce((data, byte) =>
-                        data + String.fromCharCode(byte), '')
-            );
-        base = base64String
-        });
+            .then(response => response.arrayBuffer())
+            .then(buffer => {
+                let base64String = btoa(
+                    new Uint8Array(buffer)
+                        .reduce((data, byte) =>
+                            data + String.fromCharCode(byte), '')
+                );
+                base = base64String
+            });
         let oldImg = document.querySelector("img")
-        var dataURL = `data:image/png;base64,${base}`
+        let dataURL = `data:image/png;base64,${base}`
         console.log(dataURL)
         oldImg.src = dataURL;
-
-        
-
-        } catch (error) {
+    } catch (error) {
 
     }
 }
 
+function download(url) {
+    let downloading = browser.downloads.download({
+        url,
+        filename: "AI.png",
+        conflictAction: "uniquify",
+    });
+  
+    downloading.then(onStartedDownload, onFailed);
+}
 
-setInterval(updateImg,(10*1000))
-//updateImg();
-
-var w = window.innerWidth;
-var h = window.innerHeight;
+setInterval(updateImg, (10 * 1000))
